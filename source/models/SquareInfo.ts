d8a1a28aff5b820865280acc1c937456c8587449
Wallaye <pavel.vashkevich03@gmail.com>
2023-02-21 23:08:26 +0300

    Small changes in SquareInfo.ts

--- source/models/SquareInfo.ts
+++ source/models/SquareInfo.ts
@@ -1,4 +1,18 @@
 export class SquareInfo {
   color: string | null = null
-  borderWidth: number | null = null
+  private _borderWidth: number | null = null
+  public get borderWidth() : number | null{
+    return this._borderWidth;
+  }
+  public set borderWidth(value: number | null) {
+    if (value! < 0){
+      this._borderWidth = 1;
+      return;
+    }
+    if (value! > 10){
+      this._borderWidth = 10;
+      return;
+    }
+    this._borderWidth = value;
+  }
 }
